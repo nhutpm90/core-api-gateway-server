65b1467e5c2366bd38ce7ac98bd892020eb0662a
nhut_pham <nhut.pham@ilogic.vn>
2022-07-31 13:01:42 +0700

    init proj

---
+++ src/main/java/core/mircoservices/gateway/CoreApiGatewayServerApplication.java
@@ -0,0 +1,13 @@
+package core.mircoservices.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreApiGatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreApiGatewayServerApplication.class, args);
+	}
+
+}
