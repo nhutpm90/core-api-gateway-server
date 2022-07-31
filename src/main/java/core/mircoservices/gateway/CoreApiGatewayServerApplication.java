08375de2e473bbc10a9c694e7a5f5c3e111e3875
nhut_pham <nhut.pham@ilogic.vn>
2022-07-31 15:14:42 +0700

    basic config

--- src/main/java/core/mircoservices/gateway/CoreApiGatewayServerApplication.java
+++ src/main/java/core/mircoservices/gateway/CoreApiGatewayServerApplication.java
@@ -1,13 +1,46 @@
 package core.mircoservices.gateway;
 
+import java.util.Date;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CoreApiGatewayServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CoreApiGatewayServerApplication.class, args);
 	}
 
+	@Bean
+	RouteLocator myRoutes(RouteLocatorBuilder builder) {
+	    return builder.routes()
+	        .route(p -> p
+	            .path("/my-app/test-config/**")
+	            .filters(f -> f.rewritePath("/my-app/test-config/(?<segment>.*)","/${segment}")
+	            				.addResponseHeader("X-Response-Time",new Date().toString()))
+	            .uri("lb://TEST-CONFIG"))
+	        .route(p -> p
+		            .path("/my-app/eureka-client/**")
+		            .filters(f -> f.rewritePath("/my-app/eureka-client/(?<segment>.*)","/${segment}")
+		            		.addResponseHeader("X-Response-Time",new Date().toString()))
+		            .uri("lb://EUREKA-CLIENT"))
+	        .route(p -> p
+		            .path("/my-app/api-gateway-client01/**")
+		            .filters(f -> f.rewritePath("/my-app/api-gateway-client01/(?<segment>.*)","/${segment}")
+		            				.addResponseHeader("X-Response-Time",new Date().toString()))
+		            .uri("lb://API-GATEWAY-CLIENT01"))
+	        .route(p -> p
+		            .path("/my-app/api-gateway-client02/**")
+		            .filters(f -> f.rewritePath("/my-app/api-gateway-client02/(?<segment>.*)","/${segment}")
+		            				.addResponseHeader("X-Response-Time",new Date().toString()))
+		            .uri("lb://API-GATEWAY-CLIENT02"))
+	        .build();
+	}
+	
 }
